068fab97176485eb3b95f6f3a352738669a0fa6f
Andrew <took77@bk.ru>
2022-10-27 12:46:52 +0300

    Add project files.

---
+++ Models/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Hotel
+    {
+        private ReservationBook _reservationBook;
+
+        public string Name { get; }
+
+        public Hotel(String name)
+        {
+            Name = name;
+            _reservationBook = new ReservationBook();
+        }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Reservation
+    {
+        public RoomID RoomID { get; }
+        public DateTime StartTime { get; }  // бронь с...
+
+        public DateTime EndTime { get; }    // бронь до... 
+
+        public Reservation(RoomID roomID, DateTime startTime, DateTime endTime)
+        {
+            RoomID = roomID;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    }
+}
