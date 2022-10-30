5e9896e6e3624441d84010e952345573fba03ac2
Andrew <took77@bk.ru>
2022-10-30 18:08:43 +0300

    Model modyfied

--- Models/ReservationBook.cs
+++ Models/ReservationBook.cs
@@ -8,13 +8,18 @@ namespace Reservoom.Models
 {
     public class ReservationBook
     {
-        //private readonly List<Reservation> _roomsToReservations;
-        private readonly Dictionary<RoomID, List<Reservation>> _roomsToReservations; // В будущем изменим на List<Reservation>
+        private readonly List<Reservation> _reservations;
+        //private readonly Dictionary<RoomID, List<Reservation>> _roomsToReservations; // В будущем изменим на List<Reservation>
         
         public ReservationBook()
         {
-            //_roomsToReservations = new List<Reservation>();
-            _roomsToReservations = new Dictionary<RoomID, List<Reservation>>();
+            //_roomsToReservations = new Dictionary<RoomID, List<Reservation>>();
+            _reservations = new List<Reservation>();
+        }
+
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return _reservations.Where(r => r.Username == username);
         }
     }
 }
