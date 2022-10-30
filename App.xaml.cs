c94616ba47b48aee2991f186ea02ba451c82090b
Andrew <took77@bk.ru>
2022-10-30 21:15:29 +0300

    Проверка основных методов (доменной логики) классов.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,36 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("SingletonSean Suites");
+            try
+            {
+
+            
+            hotel.MakeReservation(new Reservation(
+                new RoomID(1, 2), "SingletonSean", 
+                /* дата начала бронирования */
+                new DateTime(/* year moth day */ 2022, 10, 30 ), 
+                /* конечная дата бронирования */
+                new DateTime(2022, 11, 5)));
+
+            hotel.MakeReservation(new Reservation(
+                new RoomID(1, 3), "SingletonSean",
+                /* дата начала бронирования */
+                new DateTime(/* year moth day */ 2022, 10, 30),
+                /* конечная дата бронирования */
+                new DateTime(2022, 11, 5)));
+            }
+            catch (ReservationConflictException ex)
+            {
+
+                
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("SingletonSean");
+
+            base.OnStartup(e);
+        }
     }
 }
