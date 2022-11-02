662b04de11b7af7c3be66bfc0f365c41617f4fd8
Andrew <took77@bk.ru>
2022-11-02 16:57:22 +0300

    Navigation начало

---
+++ Stores/NavigationStore.cs
@@ -0,0 +1,21 @@
+﻿using Reservoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            // this setter is also a good place to dispose view models
+            set => _currentViewModel = value;
+
+        }
+    }
+}
